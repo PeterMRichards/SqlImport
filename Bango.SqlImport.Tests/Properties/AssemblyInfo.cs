91449feafeca57e3149715a25fdd81d8767be84d
michal-ciesielski <m.ciesielski@iesohealth.com>
2018-12-07 11:27:32 +0000

    Refactoring names

--- Bango.SqlImport.Tests/Properties/AssemblyInfo.cs
+++ Bango.SqlImport.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Bango.SqlImport.Tests")]
+[assembly: AssemblyTitle("SqlImport.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bango.SqlImport.Tests")]
+[assembly: AssemblyProduct("SqlImport.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
